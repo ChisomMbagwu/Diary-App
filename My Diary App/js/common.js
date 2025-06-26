71c662a9bf44f17b2146fd056394bcd6e6bde532
ChisomMbagwu <mimimbagwu01@gmail.com>
2025-06-26 16:24:41 +0100

    feat: Implement the create and edit diary page.

--- My Diary App/js/common.js
+++ My Diary App/js/common.js
@@ -16,7 +16,15 @@ function showToast(message, type = 'info') {
     }, 7000);
 }
 
-
+function formatDate(dateString) {
+    const options = { 
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric', 
+       
+    };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+}
 
 
 
